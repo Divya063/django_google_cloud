8bc308f2845e481dff842507b49217938869c47d
Divya Rani <ranidivya063@gmail.com>
2019-04-26 18:53:13 +0530

    added response

--- test.py
+++ test.py
@@ -14,6 +14,12 @@ string = response.content.decode('utf-8')
 json_obj = json.loads(string)
 print(json_obj)
 
+"""
+response
+
+{"date_created": "2019-04-26T02:20:20.693574", "first_name": "Name", "hash": "504138882", "id": 1, "is_default_for_billing": false, "is_default_for_shipping": false, "last_name": "Rani", "line1": "GITA college,Badaraghunathpur,Janla", "line2": "Bhubaneshwar", "line3": "", "line4": "Bhubaneshwar", "notes": "", "num_orders_as_billing_address": 0, "num_orders_as_shipping_address": 0, "phone_number": "+919162801813", "postcode": "752054", "resource_uri": "/oscarapi/v1/mymodel/1/", "search_text": "Name Rani GITA college,Badaraghunathpur,Janla Bhubaneshwar Bhubaneshwar Odisha 752054 Republic of India", "state": "Odisha", "title": "Miss"}
+"""
+
 data={
 	'first_name': "Name"
 }
