06c7ea594f1632e12a5c3a6183bbda9fa748eebc
Divya <you@example.com>
2019-04-29 06:53:59 +0530

    added example response

--- test.py
+++ test.py
@@ -27,3 +27,10 @@ data={
 response1 = session.put('http://127.0.0.1:8000/oscarapi/v1/put_request/1/', data=data)
 print(response1.content)
 
+"""
+New response (GET request)
+
+{'date_created': '2019-04-26T02:20:20.693574', 'first_name': 'Aapnik', 'hash': '92110107', 'id': 1, 'is_default_for_billing': False, 'is_default_for_shipping': False, 'last_name': 'empty', 'line1': 'GITA college,Badaraghunathpur,Janla', 'line2': 'Bhubaneshwar', 'line3': '', 'line4': 'Bhubaneshwar', 'notes': '', 'num_orders_as_billing_address': 0, 'num_orders_as_shipping_address': 0, 'phone_number': '+919162801813', 'postcode': '752054', 'resource_uri': '/oscarapi/v1/mymodel/1/', 'search_text': 'Aapnik empty GITA college,Badaraghunathpur,Janla Bhubaneshwar Bhubaneshwar Odisha 752054 Republic of India', 'state': 'Odisha', 'title': 'Miss'}
+
+"""
+
