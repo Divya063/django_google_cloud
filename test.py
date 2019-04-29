4542e1791c1a56e44cd94a6659a3284786cd7af6
Divya <you@example.com>
2019-04-29 06:51:24 +0530

    added new example

--- test.py
+++ test.py
@@ -15,7 +15,9 @@ json_obj = json.loads(string)
 print(json_obj)
 
 data={
-	'first_name': "Name"
+	'first_name': "Aapnik",
+	'last_name': "empty"
 }
-response1 = session.put('http://oscar-assignment.appspot.com/oscarapi/v1/mymodel/1/', data=data)
-#print(response.content)
+response1 = session.put('http://127.0.0.1:8000/oscarapi/v1/put_request/1/', data=data)
+print(response1.content)
+
